a75a479e92d3097b47f2ab4d6dc6f96e2c4e2ec4
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-26 10:49:10 +0800

    Create act-1.java

---
+++ shs-activities/act-1.java
@@ -0,0 +1,6 @@
+public class Main {  
+    public static void main(String[] args) {  
+        System.out.println("Welcome to the new normal");
+        System.out.println("Welcome to CRMC ICT");
+    }
+}
