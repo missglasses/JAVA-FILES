89ac585e6d81faada120a73b8190444915a34e1d
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-26 10:54:23 +0800

    Create act-2.java

---
+++ shs-activities/act-2.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Firstname: Ruby");
+        System.out.println("Middle name: Kuers");
+        System.out.println("Last name: Granger");
+        System.out.println("Course: BSIT");
+        System.out.println("Year: 3rd Year");
+        System.out.println("Address: Cebu, City");
+        System.out.println("Email: rub.books@gmail.com");
+        System.out.println("Contact Number: 09123456789");
+        System.out.println("Birthday: April 15, 2000");
+        System.out.println("Gender: Female");
+    }
+}
