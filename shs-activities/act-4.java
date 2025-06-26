3e1db630e829b22f45de08f9e7855ad051335cd4
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-26 10:56:20 +0800

    Create act-4.java

---
+++ shs-activities/act-4.java
@@ -0,0 +1,23 @@
+//APRIL 16 2021
+
+public class Main {
+    public static void main(String[] args) {
+        int cindy = 444;
+        int lepatan = 150;
+        int sum;
+        int difference;
+        int product;
+        int quotient;
+        
+        sum = cindy + lepatan;
+        difference = cindy - lepatan;
+        product = cindy * lepatan;
+        quotient = cindy / lepatan;
+        
+
+        System.out.println("The sum is: " + sum);
+        System.out.println("The difference is: " + difference);
+        System.out.println("The product is: " + product);
+        System.out.println("The quotient is: " + quotient);
+    }
+}
