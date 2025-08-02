6a2449cdaba4f03a21e6e41cdd2ad14eb58f1f5b
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-08-03 01:09:39 +0800

    Create contains().java

---
+++ review/contains().java
@@ -0,0 +1,9 @@
+class DomainChecker {
+  public static void main(String[] args) {
+    String toFind = ".gov";
+    String url = "https://www.usa.gov/holidays";
+
+
+    System.out.println(url.contains(toFind)); //<---contains()
+  }
+}
