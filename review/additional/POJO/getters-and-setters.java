c574938f41fd5cb9380d471ae66c236f969dae1d
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-08-03 21:49:34 +0800

    Create getters-and-setters.java

---
+++ review/additional/POJO/getters-and-setters.java
@@ -0,0 +1,32 @@
+//Getters
+private String make;
+
+public String getMake(){
+  get make;
+}
+
+private short on;
+public boolean isOn(){
+
+if (on=0){
+  return true;
+} else {
+return false;
+}
+}
+
+//Setters
+private String make;
+
+public String setMake(String make){
+  this.make=make;
+  }
+
+private int age;
+
+public void setAge(int age){
+  if((age>=0)&&(age<=120){
+    this.age=age;
+  }
+  
+    }
