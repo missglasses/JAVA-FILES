8ebea3f3a0ccd800821f91b8f9537d9a9fba8654
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-08-04 07:48:31 +0800

    Create createNewFile.java

---
+++ review/createNewFile.java
@@ -0,0 +1,11 @@
+
+import java.io.File;
+
+class NoteApp {
+
+    public static void main(String[] args) {
+
+        File noteFile = new File("note.txt");
+        System.out.println("Note file ready!");
+	}
+}
